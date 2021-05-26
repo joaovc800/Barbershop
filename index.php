6e2718fd7d5964643e00b683ad0b8b9c896eb120
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-05-26 15:06:14 -0300

    Create index.php
    login

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barbearia Tiko's</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body class="text-center">
+    <div class="container">
+        <form>
+            <img class="mb-4" src="imgs/logo.jpeg" width="350" height="320">
+            <div class="container col-md-4">      
+                <div class="mb-3">
+                    <input type="email" class="form-control form-control-lg" placeholder="Digite seu e-mail">
+                </div>
+            </div>
+            <div class="container col-md-4">
+                <div class="mb-3">
+                    <input type="password" class="form-control form-control-lg" placeholder="Digite sua senha">
+                </div>
+                <button type="submit" class="form-control-lg btn btn-outline-dark w-100">Acessar</button>
+                <br><br>
+                <a href="cadastrar.php">Cadastre-se</a>
+            </div>
+        </form>
+        <div class="mt-3 container-md w-50">
+            <hr>
+        </div>
+        <p class="fw-light">Barber Shop Tiko's</p>
+        <p class="fw-light">All rights reserved</p>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</body>
+</html>
