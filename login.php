bee4defca6067d3fb89c07cc94628843fe6914c7
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-05-29 12:58:21 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,20 @@
 <?php
-session_start();
 include('conexao.php');
+session_start();
 
 if(empty($_POST['usuario']) || empty($_POST['senha'])){
-    header('Location: index.php');
+    header('Location: login_usuario.php');
     exit();
 }
 
 $usuario = mysqli_real_escape_string($conexao,$_POST['usuario']); // primeiro puxa a conexao depois o usuario e proteje contra SQL injection
-$senha = mysqli_real_escape_string($conexao,($_POST['senha'])); // primeiro puxa a conexao depois a senha e proteje contra SQL injection
-$senha = md5($senha);
+$senha = mysqli_real_escape_string($conexao, md5($_POST['senha'])); // primeiro puxa a conexao depois a senha e proteje contra SQL injection
 
 $queryBuscarUsuario = "SELECT * FROM usuarios WHERE email = '{$usuario}' AND senha = md5('{$senha}')";
 
 $resultado = mysqli_query($conexao,$queryBuscarUsuario); // abre a conexão e execulta a query
+$rowID = mysqli_fetch_assoc($resultado); // Pega todas as rows
+
 $row = mysqli_num_rows($resultado); // se encontrar o resultado ela vai retornar true que é igual à 1
 
 if($row == 1){
@@ -22,7 +23,7 @@ if($row == 1){
     exit(); // fecha os cabeçalhos
 }
 else{
-    header('Location: index.php'); // se não estiver autenticado ela volta para index
+    header('Location: login_usuario.php'); // se não estiver autenticado ela volta para index
     exit();
 }
 ?>
