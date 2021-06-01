5b9e477f911859ee1835880d9de5e8182685e723
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-01 14:18:09 -0300

    Create gerenciar.php

---
+++ gerenciar.php
@@ -0,0 +1,116 @@
+<?php
+    session_start();
+    include('conexao.php');
+    include('verifica_login2.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barbearia Tiko's</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <meta property="og:site_name" content="Barbearia Tiko's">
+    <meta property="og:title" content="Barbearia Tiko's">
+    <meta property="og:description" content="Agende seu horário com as melhores disponibilidades e preços">
+    <meta name="description" content="Agende seu horário com as melhores disponibilidades e preços?">
+    <meta property="og:image" content="imgs/logo.jpeg">
+    <meta property="og:image" itemprop="image" content="https://barbeariatikos.herokuapp.com/imgs/logo.jpeg">
+    <meta property="og:image:width" content="300">
+    <meta property="og:image:height" content="300">
+    <meta property="og:url" content="https://barbeariatikos.herokuapp.com/">
+    <link rel="shortcut icon" href="imgs/logo.jpeg"/>
+    <link href="css/bootstrap-datepicker.css" rel="stylesheet">
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/bootstrap-datepicker.pt-BR.min.js"></script>
+</head>
+<body>
+    <header class="sticky-top">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid menu">
+          <a class="navbar-brand" href="#" title="Home">Barbearia Tiko's</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="administrador.php">Ver agendamentos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="gerenciar.php">Gerenciar</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="logout.php">Sair</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <div class="mt-5 container card">
+      <div class="card-header bg-dark">
+        <h2 class="card-title text-center text-light">Selecione a data para excluir o agendamentos</h2>
+      </div>
+      <div class="card-body">
+        <form action="gerenciamento.php" method="POST">
+              <div class="container">
+                  <div class="row">
+                      <input type="text" class="form-control" id="data" name="data_excluir" placeholder="Selecione uma data">
+                      <button class="btn btn-danger mt-2">Excluir</button>
+                  </div>
+              </div>
+          </form>
+          <?php
+          if($_SESSION['deletado']){
+          ?>
+          <div class="container">
+            <p class="text-center text-danger"><?php echo "Agendamentos referente a data escolhida foram deletados!"?></p>
+          </div>
+          <?php
+            }
+            unset($_SESSION['deletado'])
+          ?>
+          <?php
+          if($_SESSION['data_vazia']){
+          ?>
+          <div class="container">
+            <p class="text-center text-danger">O campo acima não pode ficar vazio!</p>
+          </div>
+          <?php
+            }
+            unset($_SESSION['data_vazia'])
+          ?>
+          <?php
+          if($_SESSION['nao_deletado']){
+          ?>
+          <div class="container">
+            <p class="text-center text-danger">Não temos agendamento para esta data</p>
+          </div>
+          <?php
+            }
+            unset($_SESSION['nao_deletado'])
+          ?>
+      </div>
+    </div>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/bootstrap-datepicker.pt-BR.min.js"></script>
+    
+    <script>
+      $('#data').datepicker({
+      format: 'dd/mm/yyyy',
+      language: "pt-BR",
+      //startDate: "+0d",
+      daysOfWeekDisabled: "0",
+      autoclose: 1,
+      });
+    </script>
+  </body>
+</html>
