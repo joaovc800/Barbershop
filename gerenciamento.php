58d0bafe5afcb62a390051b591c15cbeed68e8ff
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-01 18:58:47 -0300

    Update gerenciamento.php

--- gerenciamento.php
+++ gerenciamento.php
@@ -9,7 +9,7 @@
         exit();
      }
 
-    $query = "DELETE FROM agendamentos WHERE data = '$data'";
+    $query = "SET SQL_SAFE_UPDATES=0; DELETE FROM agendamentos WHERE data = '$data'";
     
     $deletar = mysqli_query($conexao,$query);
 
