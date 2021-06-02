009ec4634fc5aed3b9f336e57a623325b56ff408
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-01 21:34:55 -0300

    Update agendamento.php

--- agendamento.php
+++ agendamento.php
@@ -40,11 +40,39 @@ if($conexao->query($sql) === TRUE) {
 
 $conexao->close();
 
-$vmail = "joao.costa.costa@uni9.edu.br";
-$assunto = "Novo agendamento no site da Tiko's";
-$vmsg = "Um novo agendamento foi realizado no site por ".$nome;
+require_once('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
 
-mail($vmail,$assunto,$vmsg);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try{
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'joao.costa.costa@uni9.edu.br';
+    $mail->Password = '13959624';
+    $mail->Port = 587;
+
+    $mail->setFrom('joaoviniciusdacosta800@gmail.com');
+	//$mail->addAddress('endereco1@provedor.com.br');
+	//$mail->addAddress('endereco2@provedor.com.br');
+ 
+	$mail->isHTML(true);
+	$mail->Subject = 'Novo agendamento';
+	$mail->Body = 'Houve um novo agendamento no <strong>Barbearia Tikos</strong>';
+	$mail->AltBody = 'Houve um novo agendamento no Barbearia Tikos';
+
+
+
+} catch (Exception $e){
+    echo "Erro ao enviar ao enviar mensagem: {$mail->ErrorInfo}";
+}
 
 header('Location: agendar.php');
 exit();
