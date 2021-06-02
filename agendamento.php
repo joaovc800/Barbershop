744fdc80acd4f9756f4784684b6b07cb36e75fad
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-01 21:47:56 -0300

    Update agendamento.php

--- agendamento.php
+++ agendamento.php
@@ -2,40 +2,6 @@
 session_start();
 include('conexao.php');
 
-require_once('src/PHPMailer.php');
-require_once('src/SMTP.php');
-require_once('src/Exception.php');
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-$mail = new PHPMailer(true);
-
-try{
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'joaoviniciusdacosta800@gmail.com';
-    $mail->Password = '13959624@v';
-    $mail->Port = 587;
-
-    $mail->setFrom('joaoviniciusdacosta800@gmail.com');
-	//$mail->addAddress('endereco1@provedor.com.br');
-	//$mail->addAddress('endereco2@provedor.com.br');
- 
-	$mail->isHTML(true);
-	$mail->Subject = 'Novo agendamento';
-	$mail->Body = 'Houve um novo agendamento no <strong>Barbearia Tikos</strong>';
-	$mail->AltBody = 'Houve um novo agendamento no Barbearia Tikos';
-
-
-
-} catch (Exception $e){
-    echo "Erro ao enviar ao enviar mensagem: {$mail->ErrorInfo}";
-}
-
 if(empty($_POST['nome_completo']) || empty($_POST['agendamento']) || empty($_POST['horario'])){
     $_SESSION['campos_vazios'] = true;
     header('Location: agendar.php');
